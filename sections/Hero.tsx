5a4ea00a755eec75dd7cf30197cbee0b3017a91e
0xNunana <kudayapaul@gmail.com>
2024-01-28 12:28:44 +0000

    hero animation added

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -12,14 +12,14 @@ const Hero = () => {
          {/* <Image src='/jeep.webp' alt='jeep' width={3000} height={300}/> */}
      {/* <Booking/> */}
      <div className='flex flex-col justify-center items-center h-[80%] '>
-        <p className='max-sm:pl-3 text-5xl md:text-6xl lg:text-7xl text-white font-extrabold'>Your Adventure Begins here!</p>
+        <p className='max-sm:pl-3 text-5xl md:text-6xl lg:text-7xl text-white font-extrabold animate-slidein1 '>Your Adventure Begins here!</p>
 
      </div>
      <div className='absolute bottom-10 right-0 left-0 flex justify-center items-center text-center flex-wrap'>
         <div className='space-y-2'>
-        <p className='max-w-2xl font-semibold md:text-2xl text-white  p-3'>Discover the magic of long drives and camping. Connect with nature in it&apos;s purest form and awaken your senses as you navigate through rugged trails, lush forests and wild animals </p>
+        <p className='max-w-2xl font-semibold md:text-2xl text-white  p-3 animate-slidein2'>Discover the magic of long drives and camping. Connect with nature in it&apos;s purest form and awaken your senses as you navigate through rugged trails, lush forests and wild animals </p>
      <div className='flex justify-center items-center'>
-     <p className='bg-white rounded-full w-40 px-5 py-2 font-semibold'>Reserve a spot</p>
+     <p className='bg-white rounded-full w-40 px-5 py-2 font-semibold animate-slidein3'>Reserve a spot</p>
      </div>
 
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,32 @@ const config: Config = {
        
           "hero":"url('/jeep2.webp')"
       },
+      keyframes:{
+        slidedown:{
+          from:{
+            opacity:'0',
+            transform: 'translateY(-50px)' 
+          },
+          to:{
+opacity:'1',
+transform: 'translateY(0)' 
+          }
+        },slideup:{
+          from:{
+            opacity:'0',
+            transform: 'translateY(50px)' 
+          },
+          to:{
+opacity:'1',
+transform: 'translateY(0)' 
+          }
+        }
+      },
+      animation:{
+        'slidein1':'slidedown 2s ease forwards',
+        'slidein2':'slidedown 4s ease forwards',
+        'slidein3':'slideup 6s ease forwards'
+      }
     },
   },
   plugins: [],
